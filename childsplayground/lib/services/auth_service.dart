6141c6ce7ae0aaa0108bbe59f87fcb6877647103
rhmnnbl26 <Rahmannabil766@gmail.com>
2025-11-23 22:24:02 +0700

    start over

--- childsplayground/lib/services/auth_service.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-import 'package:crypto/crypto.dart';
-
-String hashPassword(String password) {
-  final bytes = utf8.encode(password);
-  final digest = sha256.convert(bytes);
-  return digest.toString();
-}
---
+++ mainan_anak/android/app/src/main/kotlin/com/example/mainan_anak/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mainan_anak
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
